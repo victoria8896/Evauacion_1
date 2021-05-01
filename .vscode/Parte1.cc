2b92d432de0cf59ca17bae6b7f0de6a75349c3f7
Wiston <reyeswiston8@gmail.com>
2021-05-01 02:17:30 -0400

    parte1resuelta

--- .vscode/Parte1.cc
+++ .vscode/Parte1.cc
@@ -1,4 +1,4 @@
-/*1.Realizar un programa en c++ obedeciendo a la siguiente argumentación:
+/*1.Realizar un programa en c++ obedeciendo a la siguiente argumentacion:
 El costo de la carrera del IUTA puede ser cancelado de la siguiente manera : 
 una inicla del 20% del costo completo y el resto debe ser cancelado 
 en cuotas mensuales durante 3 años, imprima un conjunto de instrucciones 
@@ -6,16 +6,15 @@ que permita calcular e imprimir la inicial y el monto de las cuotas mensuales
 dado el costo de la carrera*/
 #include <iostream>
 using namespace std;
-int costo, inicial,monto;
+int costo,inicial;
 float cuota;
 int main(){
-       cout<<"\nIngrese el costo de la carrera: " << endl; 
+       cout<<"\nIngrese el costo de la carrera: "; 
        cin >> costo;
-       inicial = (costo)%20*100;
-       cout<<"\nEl costo inicial de la carrera es: "; 
-       cout<< inicial;
-       cuota = (costo-inicial)/36;
-       cout<<"\nLa cuota mensual a lo largo de los 36 meses de duracion de la carrera seria: ";
-       cout<< cuota;
-       
+       inicial = costo * 0.20;       
+       cuota = ((float)costo-inicial) / 36;
+         cout<<"La cuota durante los 3 años es: " << cuota;
+       cout<<"\nEl costo inicial de la carrera es: " <<inicial;
+       cout << "\nLa cuota durante los 3 años es: " << cuota;
+      return 0;
 }
