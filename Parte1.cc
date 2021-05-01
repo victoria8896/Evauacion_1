357ea0802581861071f4c31f5f4b4431874be28c
Wiston <reyeswiston8@gmail.com>
2021-04-30 22:28:39 -0400

    d

---
+++ Parte1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    printf( "\n   Introduzca un n%cmero entero: ", 163 );
+    scanf( "%d", &numero );
+
+    if ( numero % 2 == 0 )
+        printf( "\n   EL N%MERO INTRODUCIDO ES PAR" );
+    else
+        printf( "\n   EL N%MERO INTRODUCIDO ES IMPAR" );
+
+
+    return 0;
+}
